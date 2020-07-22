18d782fabc4cdfe94e6088240948b9efe0889bbd
xhe <xw897002528@gmail.com>
2020-07-22 15:52:26 +0800

    ddl: fix tests due to previous suggestion commit

--- ddl/db_test.go
+++ ddl/db_test.go
@@ -5148,7 +5148,7 @@ add placement policy
 	constraints='+zone=sh'
 	role=leader
 	replicas=3`)
-	c.Assert(err, ErrorMatches, ".*Alter partition 'p' on an unpartioned table")
+	c.Assert(err, ErrorMatches, ddl.ErrPartitionMgmtOnNonpartitioned)
 }
 
 func init() {
