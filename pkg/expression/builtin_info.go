56c57675615e845b66cf9671433cec320c5282e1
tianyuan <tianyuan@pingcap.cn>
2025-07-14 22:47:15 +0800

    cherry-pick commits from release-7.1.8-5 to pingkai-master (#125)
    Reviewed-on: https://git.pingcap.net/pingkai/tidb/pulls/125
    Reviewed-by: zhaoyilin <zhaoyilin@pingcap.cn>

--- pkg/expression/builtin_info.go
+++ pkg/expression/builtin_info.go
@@ -42,6 +42,7 @@ import (
 	"github.com/pingcap/tidb/pkg/util/dbterror/plannererrors"
 	"github.com/pingcap/tidb/pkg/util/logutil"
 	"github.com/pingcap/tidb/pkg/util/plancodec"
+	"github.com/pingcap/tidb/pkg/util/printer"
 	"github.com/pingcap/tipb/go-tipb"
 	"go.uber.org/zap"
 )
@@ -570,7 +571,7 @@ func (c *tidbVersionFunctionClass) getFunction(ctx BuildContext, args []Expressi
 	if err != nil {
 		return nil, err
 	}
-	bf.tp.SetFlen(len(VersionPrinter))
+	bf.tp.SetFlen(len(printer.GetTiDBInfo()))
 	sig := &builtinTiDBVersionSig{bf}
 	return sig, nil
 }
@@ -591,7 +592,7 @@ func (b *builtinTiDBVersionSig) Clone() builtinFunc {
 // evalString evals a builtinTiDBVersionSig.
 // This will show git hash and build time for tidb-server.
 func (b *builtinTiDBVersionSig) evalString(ctx EvalContext, row chunk.Row) (string, bool, error) {
-	return VersionPrinter, false, nil
+	return printer.GetTiDBInfo(), false, nil
 }
 
 type dataOpAuditFunctionClass struct {
--- pkg/sessionctx/variable/pkdb_sysvar.go
+++ pkg/sessionctx/variable/pkdb_sysvar.go
@@ -5,7 +5,9 @@ package variable
 
 import (
 	"context"
+	"sync/atomic"
 
+	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/pkg/sessionctx/vardef"
 	"github.com/pingcap/tidb/pkg/util/logutil"
 	"github.com/tikv/client-go/v2/tikvrpc"
@@ -15,17 +17,19 @@ import (
 // FFI-dependent system variables
 var ffiSysVars = []*SysVar{
 	{Scope: vardef.ScopeGlobal, Name: vardef.TiDBXEnableTiKVLocalCall, Value: BoolToOnOff(vardef.DefTiDBXEnableLocalRPCOpt), Type: vardef.TypeBool,
-		SetGlobal: func(_ context.Context, vars *SessionVars, s string) error {
+		SetGlobal: func(_ context.Context, _ *SessionVars, s string) error {
 			if TiDBOptOn(s) != tikvrpc.EnableTiKVLocalCall.Load() {
 				tikvrpc.EnableTiKVLocalCall.Store(TiDBOptOn(s))
-				logutil.BgLogger().Info("set enable local rpc opt", zap.Bool("enable", TiDBOptOn(s)))
+				logutil.BgLogger().Info("set enable local rpc opt",
+					zap.String("variable", TiDBXEnableTiKVLocalCall),
+					zap.Bool("enable", TiDBOptOn(s)))
 			}
 			return nil
-		}, GetGlobal: func(_ context.Context, vars *SessionVars) (string, error) {
-			return BoolToOnOff(tikvrpc.EnableTiKVLocalCall.Load()), nil
-		}},
+		}, GetGlobal: func(_ context.Context, _ *SessionVars) (string, error) {
+		return BoolToOnOff(tikvrpc.EnableTiKVLocalCall.Load()), nil
+	}},
 	{Scope: vardef.ScopeGlobal, Name: vardef.TiDBXEnableScheduleLeaderRule, Value: BoolToOnOff(vardef.DefTiDBXEnableScheduleLeaderRule), Type: vardef.TypeBool,
-		SetGlobal: func(_ context.Context, vars *SessionVars, s string) error {
+		SetGlobal: func(_ context.Context, _ *SessionVars, s string) error {
 			v := TiDBOptOn(s)
 			if v != vardef.EnableScheduleLeaderRule.Load() {
 				vardef.EnableScheduleLeaderRule.Store(v)
@@ -34,10 +38,33 @@ var ffiSysVars = []*SysVar{
 				}
 			}
 			return nil
-		}, GetGlobal: func(_ context.Context, vars *SessionVars) (string, error) {
-			return BoolToOnOff(vardef.EnableScheduleLeaderRule.Load()), nil
-		}},
+		}, GetGlobal: func(_ context.Context, _ *SessionVars) (string, error) {
+		return BoolToOnOff(vardef.EnableScheduleLeaderRule.Load()), nil
+	}},
+	{Scope: vardef.ScopeGlobal, Name: vardef.TiDBXEnablePDLocalCall, Value: BoolToOnOff(vardef.DefTiDBXEnableLocalRPCOpt), Type: vardef.TypeBool,
+		SetGlobal: func(_ context.Context, _ *SessionVars, s string) error {
+			if PDLocalCallVar == nil {
+				return errors.Errorf("%s is not initialized. Please check if this is fusion mode", vardef.TiDBXEnablePDLocalCall)
+			}
+			if TiDBOptOn(s) != (*PDLocalCallVar).Load() {
+				(*PDLocalCallVar).Store(TiDBOptOn(s))
+				logutil.BgLogger().Info("set enable local rpc opt",
+					zap.String("variable", vardef.TiDBXEnablePDLocalCall),
+					zap.Bool("enable", TiDBOptOn(s)))
+			}
+			return nil
+		}, GetGlobal: func(context.Context, *SessionVars) (string, error) {
+		if PDLocalCallVar == nil {
+			return "", errors.Errorf("%s is not initialized. Please check if this is fusion mode", vardef.TiDBXEnablePDLocalCall)
+		}
+		return BoolToOnOff((*PDLocalCallVar).Load()), nil
+	}},
 }
+
+// PDLocalCallVar will be set by the upper package tidbx-server to point to pd-server's
+// EnableIPC. This is to break the dependency cycle between tidbx-server and
+// pd-server.
+var PDLocalCallVar *atomic.Bool
 
 func init() {
 	defaultSysVars = append(defaultSysVars, ffiSysVars...)
