e959ffb351a2a3f00351cdae5488787837676407
何芊蔚 <qianwei.he@pingcap.cn>
2025-07-30 00:39:38 +0800

    Merge branch 'pingkai-master', fix pkg/parser/parser.go, pkg/planner/core/physical_plan_test.go, pkg/planner/core/planbuilder.go, pkg/session/session.go, pkg/sessionctx/vardef/tidb_vars.go, pkg/sessionctx/variable/session.go, pkg/util/signal/signal_posix.go conflicts

--- pkg/planner/core/exhaust_physical_plans.go
+++ pkg/planner/core/exhaust_physical_plans.go
@@ -1527,6 +1527,7 @@ func constructDS2TableScanTask(
 		isPartition:     ds.PartitionDefIdx != nil,
 		tblCols:         ds.TblCols,
 		tblColHists:     ds.TblColHists,
+		TableSplit:      ds.TableSplit,
 	}.Init(ds.SCtx(), ds.QueryBlockOffset())
 	ts.SetSchema(ds.Schema().Clone())
 	if rowCount <= 0 {
@@ -1770,6 +1771,7 @@ func constructDS2IndexScanTask(
 			physicalTableID: ds.PhysicalTableID,
 			tblCols:         ds.TblCols,
 			tblColHists:     ds.TblColHists,
+			TableSplit:      ds.TableSplit,
 		}.Init(ds.SCtx(), ds.QueryBlockOffset())
 		ts.SetSchema(is.dataSourceSchema.Clone())
 		if ds.TableInfo.IsCommonHandle {
