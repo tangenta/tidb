4f64fe9099cf51104fae25a423f40994c26abf12
crazycs520 <chenshuang@pingcap.cn>
2025-10-13 19:51:30 +0800

    fix tidbx-server build (!356)
    Reviewed-on: https://git.pingcap.net/pingkai/tidb/pulls/356

--- pkg/pkdb-tidb-server/pkdb_xmain.go
+++ pkg/pkdb-tidb-server/pkdb_xmain.go
@@ -913,7 +913,7 @@ func setupTracing() {
 func closeDDLOwnerMgrDomainAndStorage(storage kv.Storage, dom *domain.Domain) {
 	tikv.StoreShuttingDown(1)
 	dom.Close()
-	ddl.CloseOwnerManager()
+	ddl.CloseOwnerManager(storage)
 	copr.GlobalMPPFailedStoreProber.Stop()
 	mppcoordmanager.InstanceMPPCoordinatorManager.Stop()
 	err := storage.Close()
